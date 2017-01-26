6dcc843eaeae3c7376b2810dabab8533d9754245
Alexandre Resende <alexandre.amres@gmail.com>
2017-01-25 22:32:01 -0200

    todo list + landingpage

--- ToDoList/script/app.js
+++ ToDoList/script/app.js
@@ -19,25 +19,23 @@ var todoList = {
   toggleAll: function() {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
-    
+
     // Get number of completed todos.
-    for (var i = 0; i < totalTodos; i++) {
-      if (this.todos[i].completed === true) {
+    this.todos.forEach(function(todo) {
+      if (todo.completed === true) {
         completedTodos++;
       }
-    }
+    });
     
-    // Case 1: If everything’s true, make everything false.
-    if (completedTodos === totalTodos) {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = false;
+    this.todos.forEach(function(todo) {
+      // Case 1: If everything's true, make everything false.
+      if (completedTodos === totalTodos) {
+        todo.completed = false;
+      // Case 2: Otherwise, make everything true.
+      } else {
+        todo.completed = true;
       }
-    // Case 2: Otherwise, make everything true.
-    } else {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = true;
-      }      
-    }
+    });
   }
 };
 
@@ -76,9 +74,9 @@ var view = {
   displayTodos: function() {
     var todosUl = document.querySelector('ul');
     todosUl.innerHTML = '';
-    for (var i = 0; i < todoList.todos.length; i++) {
+    
+    todoList.todos.forEach(function(todo, position) {
       var todoLi = document.createElement('li');
-      var todo = todoList.todos[i];
       var todoTextWithCompletion = '';
 
       if (todo.completed === true) {
@@ -87,35 +85,32 @@ var view = {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
       
-      todoLi.id = i;
+      todoLi.id = position;
       todoLi.textContent = todoTextWithCompletion;
       todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
-    }
+    }, this);
   },
-  createDeleteButton: function(){
+  createDeleteButton: function() {
     var deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
     deleteButton.className = 'deleteButton';
     return deleteButton;
   },
-  setUpEventListener: function(){
-    var todoUl = document.querySelector('ul');
-
-    todoUl.addEventListener('click', function(event){  
-      //get the element that was clicked on
+  setUpEventListeners: function() {
+    var todosUl = document.querySelector('ul');
+    
+    todosUl.addEventListener('click', function(event) {
+      // Get the element that was clicked on.
       var elementClicked = event.target;
-
-      //check if element clicked is a deleteButton
-      if (elementClicked.className === 'deleteButton'){
-        //run handlers.deleteTodo
+      
+      // Check if elementClicked is a delete button.
+      if (elementClicked.className === 'deleteButton') {
         handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
       }
-    });
+    });    
   }
 };
 
-view.setUpEventListener();
-
-
+view.setUpEventListeners();
 
---
+++ landingPage/readme.md
@@ -0,0 +1,5 @@
+
+
+###Simple landing page Project
+
+It was proposed by Kenneth Amanfo from Ghana
